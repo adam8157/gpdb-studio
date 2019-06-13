db8a5eb590132e976007a4e0e7f9cee76655942d
Adam Lee <ali@pivotal.io>
2019-06-13 13:48:15 +0800

    Disable PXF and gpcloud by default

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ export PATH="/usr/local/opt/apr/bin:$PATH"
 export LDFLAGS="-L/usr/local/opt/openssl/lib"
 export CPPFLAGS="-I/usr/local/opt/openssl/include"
 
-CFLAGS="-O0 -g3" ./configure --prefix=`echo ~`/greenplum-db-devel --with-gssapi --with-openssl --with-libxml --enable-cassert --enable-debug --enable-gpfdist --disable-orca
+CFLAGS="-O0 -g3" ./configure --prefix=`echo ~`/greenplum-db-devel --with-python --with-gssapi --with-openssl --with-libxml --enable-cassert --enable-debug --enable-gpfdist --disable-gpcloud --disable-pxf --disable-orca
 
 make -j2
