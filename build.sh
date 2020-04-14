c7fe39ee1a14ac97afb14d29e864ffa23d5210de
Adam Lee <adam8157@gmail.com>
2020-04-14 16:30:05 +0800

    Enable zstd and gpfdist

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 # extra environment variables for macOS
 export PATH="/usr/local/opt/apr/bin:$PATH"
-export LDFLAGS="-L/usr/local/opt/openssl/lib"
-export CPPFLAGS="-I/usr/local/opt/openssl/include"
+export PATH="/usr/local/opt/apr-util/bin:$PATH"
+export LDFLAGS="-L/usr/local/opt/zstd/lib -L/usr/local/opt/libevent/lib -L/usr/local/opt/openssl/lib"
+export CPPFLAGS="-I/usr/local/opt/zstd/include -I/usr/local/opt/libevent/include -I/usr/local/opt/openssl/include"
 
-CFLAGS="-O0 -g3" ./configure --prefix=`echo ~`/greenplum-db-devel --with-python --with-gssapi --with-openssl --with-libxml --enable-depend --enable-cassert --enable-debug --enable-gpfdist --disable-gpcloud --disable-pxf --disable-orca
+CFLAGS="-O0 -g3" ./configure --prefix=`echo ~`/greenplum-db-devel --with-python --with-gssapi --with-openssl --with-libxml --with-zstd --enable-depend --enable-cassert --enable-debug --enable-gpfdist --disable-gpcloud --disable-pxf --disable-orca
 
 make -j2
