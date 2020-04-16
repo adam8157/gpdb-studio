5c9bb9afb14b761108299a43fbf936875b72a343
Adam Lee <ali@pivotal.io>
2020-04-16 12:08:42 +0800

    For compilers to find libxml2 on macOS

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 # extra environment variables for macOS
-export PATH="/usr/local/opt/apr/bin:$PATH"
-export PATH="/usr/local/opt/apr-util/bin:$PATH"
-export LDFLAGS="-L/usr/local/opt/zstd/lib -L/usr/local/opt/libevent/lib -L/usr/local/opt/openssl/lib"
-export CPPFLAGS="-I/usr/local/opt/zstd/include -I/usr/local/opt/libevent/include -I/usr/local/opt/openssl/include"
+export PATH="/usr/local/opt/apr/bin:/usr/local/opt/apr-util/bin:/usr/local/opt/libxml2/bin:$PATH"
+export LDFLAGS="-L/usr/local/opt/zstd/lib -L/usr/local/opt/libevent/lib -L/usr/local/opt/openssl/lib -L/usr/local/opt/libxml2/lib"
+export CPPFLAGS="-I/usr/local/opt/zstd/include -I/usr/local/opt/libevent/include -I/usr/local/opt/openssl/include -I/usr/local/opt/libxml2/include"
 
 CFLAGS="-O0 -g3" ./configure --prefix=`echo ~`/greenplum-db-devel --with-python --with-gssapi --with-openssl --with-libxml --with-zstd --enable-depend --enable-cassert --enable-debug --enable-gpfdist --disable-gpcloud --disable-pxf --disable-orca
 
