cd5fa291ff6b9e29e56d836b853a5ec0c92174aa
Adam Lee <adlee@vmware.com>
2020-09-02 15:29:24 +0800

    Disable IC proxy

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ export PATH="/usr/local/opt/apr/bin:/usr/local/opt/apr-util/bin:/usr/local/opt/l
 export LDFLAGS="-L/usr/local/opt/zstd/lib -L/usr/local/opt/libevent/lib -L/usr/local/opt/openssl/lib -L/usr/local/opt/libxml2/lib"
 export CPPFLAGS="-I/usr/local/opt/zstd/include -I/usr/local/opt/libevent/include -I/usr/local/opt/openssl/include -I/usr/local/opt/libxml2/include"
 
-CFLAGS="-O0 -g3" ./configure --prefix=`echo ~`/greenplum-db-devel --with-python --with-gssapi --with-openssl --with-libxml --with-zstd --enable-depend --enable-cassert --enable-debug --enable-gpfdist --disable-gpcloud --disable-pxf --disable-orca
+CFLAGS="-O0 -g3" ./configure --prefix=`echo ~`/greenplum-db-devel --with-python --with-gssapi --with-openssl --with-libxml --with-zstd --enable-depend --enable-cassert --enable-debug --enable-gpfdist --disable-gpcloud --disable-pxf --disable-orca --disable-ic-proxy
 
 if command -v gmake >/dev/null 2>&1; then
 	bear gmake -j2
