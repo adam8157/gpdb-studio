82c62e419fcd170889e3f67a83771974f8d84619
Adam Lee <adlee@vmware.com>
2022-02-21 13:45:37 +0800

    Be compatible with different homebrew prefix

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 # extra environment variables for macOS
-export PATH="/usr/local/opt/apr/bin:/usr/local/opt/apr-util/bin:/usr/local/opt/libxml2/bin:$PATH"
-export LDFLAGS="-L/usr/local/opt/zstd/lib -L/usr/local/opt/libevent/lib -L/usr/local/opt/openssl/lib -L/usr/local/opt/libxml2/lib"
-export CPPFLAGS="-I/usr/local/opt/zstd/include -I/usr/local/opt/libevent/include -I/usr/local/opt/openssl/include -I/usr/local/opt/libxml2/include"
+if command -v brew >/dev/null 2>&1; then
+	export PATH="`brew --prefix`/opt/apr/bin:`brew --prefix`/opt/apr-util/bin:`brew --prefix`/opt/libxml2/bin:$PATH"
+	export LDFLAGS="-L`brew --prefix`/opt/zstd/lib -L`brew --prefix`/opt/libevent/lib -L`brew --prefix`/opt/openssl/lib -L`brew --prefix`/opt/libxml2/lib"
+	export CPPFLAGS="-I`brew --prefix`/opt/zstd/include -I`brew --prefix`/opt/libevent/include -I`brew --prefix`/opt/openssl/include -I`brew --prefix`/opt/libxml2/include"
+fi
 
 CFLAGS="-O0 -g3" ./configure --prefix=`echo ~`/greenplum-db-devel --disable-gpcloud --disable-ic-proxy --disable-orca --disable-pxf --enable-cassert --enable-debug --enable-depend --enable-gpfdist --enable-tap-tests --with-gssapi --with-libxml --with-openssl --with-python --with-zstd
 
